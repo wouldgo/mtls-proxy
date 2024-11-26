951e2f9e961e281105ba2909f75d394dbd2446f2
wouldgo <wouldgo84@gmail.com>
2024-11-26 18:04:40 +0100

    install script issue

--- install_local_ca.sh
+++ install_local_ca.sh
@@ -7,16 +7,20 @@ function do_it () {
   local FULL_CA_CERTIFICATES_PATH
   local CA_FILE_PATH
 
+  local CERT_FILENAME
+
   CA_CERTIFICATE_FOLDER="mtls_proxy_ca"
   LOCAL_CA_FILE_PATH=${1:-_fake_pki/_ca/certificate.pem}
 
+  CERT_FILENAME="ca.crt"
+
   FULL_CA_CERTIFICATES_PATH="/usr/share/ca-certificates/${CA_CERTIFICATE_FOLDER}"
-  CA_FILE_PATH="${FULL_CA_CERTIFICATES_PATH}/ca.crt"
+  CA_FILE_PATH="${FULL_CA_CERTIFICATES_PATH}/${CERT_FILENAME}"
 
   mkdir "${FULL_CA_CERTIFICATES_PATH}"
   cp "${LOCAL_CA_FILE_PATH}" "${CA_FILE_PATH}"
 
-  echo "${CA_FILE_PATH}" | tee -a /etc/ca-certificates.conf
+  echo "${CA_CERTIFICATE_FOLDER}/${CERT_FILENAME}" | tee -a /etc/ca-certificates.conf
 
   update-ca-certificates
 }
